9ef7a7066fb361dafb863fff6c702e90b39ec6a7
joehuang1108 <joehuang1108@gmail.com>
2023-12-17 17:36:02 -0800

    completed hangman game

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import random
+import webbrowser
+import requests
+import time
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import re
 
 # Hangman
 # guess the word game
@@ -10,35 +16,55 @@ import random
 # else: tries -= 1
 # repeat until word = guessed or out of tries
 
-
-word_bank = ["water", "cooler", "summer"]
-word = random.choice(word_bank)
-print("_" * len(word))
-
-# this holds the display of hidden word
-
-# this holds onto all the letters that was guessed
-guess_made = ""
-chances = 5
-# while(chances > 0):
-#     main = ""
-#     guess = input("enter a guess: ")
-#     guess_made += guess
-#     for letter in word:
-#         if letter in guess_made:
-#             main = main + letter
-#         else:
-#             main += "_"
-#     print(main)
-#
-#     if guess not in word:
-#         chances -= 1
-
-print("Hello")
-print("World")
+# CHECK for invalid entries
+# MAKE bigger word bank
 
 
+def hangman():
+    word_bank = ["water", "cooler", "summer"]
+    word = random.choice(word_bank)
+    print("_" * len(word))
+    valid_letters = "abcdefghijklmnopqrstuvwxyz"
 
-# summer
-# sum
-# __m
+    guess_made = ""
+    chances = 5
+    while(chances > 0):
+        main = ""
+        guess = input("enter a guess: ")
+        guess = guess.lower()
+
+        while (guess not in valid_letters) or (guess in guess_made):
+            guess = input("enter a guess: ")
+            guess = guess.lower()
+
+        guess_made += guess
+        for letter in word:
+            if letter in guess_made:
+                main = main + letter
+            else:
+                main += "_"
+        if main == word:
+            print("win")
+            break
+        print(main)
+
+        if guess not in word:
+            chances -= 1
+            print("chances" + str(chances))
+
+
+def get_random_words():
+    url = 'https://www.randomlists.com/random-words'
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        words = soup.find_all('span.rand_large')
+    else:
+        print(f"Failed to retrieve the page. Status code: {response.status_code}")
+        return None
+    print(words)
+
+hangman()
+# get_random_words()
